9d492bef24eab9bfe7dbba2c185c1ea20f3e8c69
Dennis <dmyasnyankin@berkeley.edu>
2018-12-12 11:31:30 -0800

    Add controllers(user, session), routes, set up frontend folder

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+    def new  
+    end
+
+    def create
+        user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        if user
+            login(user)
+            # redirect_to
+        else
+            flash.now[:errors] = "Email or password Invalid"
+            render :new
+        end
+    end
+
+    def destroy
+        logout
+        redirect_to new_session_url
+    end
 end
