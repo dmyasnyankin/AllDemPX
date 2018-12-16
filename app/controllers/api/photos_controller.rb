385b0c4d9cf0d32505338f8dd58b9b5606446040
Dennis <dmyasnyankin@berkeley.edu>
2018-12-16 15:23:20 -0800

    Create photos controller

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,5 +1,25 @@
 class Api::PhotosController < ApplicationController
     before_action :require_login, only: [:create]
 
-    
+    def index
+        @photo = Photo.all
+    end
+
+    def show
+        @photo = Photo.find(params[:id])
+    end
+
+    def create
+        @photo = Photo.new(photo_params)
+
+        if @photo.save
+            render "api/photos"
+        else
+            render json: @photo.errors.full_messages, status: 422
+        end
+    end
+
+    def photo_params
+        params.require(:photos).permit(:title, :image_url, :author_id)
+    end
 end
