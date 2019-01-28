8e0ee1cd57266b4ef4fe3806a1c5bb784ab4f75f
Dennis Myasnyankin <dennis415@live.com>
2019-01-28 09:38:50 -0800

    Style splash page button and add new links

--- frontend/components/pretty_front_page.jsx
+++ frontend/components/pretty_front_page.jsx
@@ -3,23 +3,41 @@ import { Link } from 'react-router-dom';
 
 const PrettyFrontPage = () => {
     return (
-        <div>
-            <h1 className="main-welcome">Experience and be the lens of inspiration, sharing it with the world</h1>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-            <h3 className="main-welcome-description">Your home away from home</h3>
-            <br/>
-            <Link to="/signup">
-                <div className="pretty-front-page-button">
-                <p>Join PXtures</p>
-                </div>
-            </Link>
+        // <div>
+        //     <h1 className="main-welcome">Experience and be the lens of inspiration, sharing it with the world</h1>
+        //         <br/>
+        //         <br/>
+        //         <br/>
+        //         <br/>
+        //         <br/>
+        //         <br/>
+        //         <br/>
+        //         <br/>
+        //     <h3 className="e">Your home away from home</h3>
+        //     <br/>
+        //     <Link to="/signup">
+        //         <div className="pretty-front-page-button">
+        //         <p>Join PXtures</p>
+        //         </div>
+        //     </Link>
+        // </div>
+
+        <div className="splash-page">
+            <div className="splash-page-image">
+                <img src="" alt=""/>
+            </div>
+            <div className="splash-nav-left">
+                <Link to="/signup"><button>
+                    Sign Up
+                </button></Link>
+            </div>
+            <div className="splash-nav-right"></div>
+            <h1 className="splash-main-quote">Experience and be the lens of inspiration, sharing it with the world</h1>
+            <h2 className="splash-quote-des">Your home away from home</h2>
+            <Link className="splash-link" to="/signup"><button className="splash-button">Join Now!
+
+            </button></Link>
+            <h1></h1>
         </div>
     )
 
--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,45 +1,21 @@
 
-const Greeting = () => (
-    <div className="tip_top_logo">
-        <div className="all-dem-px">
-            <img src={window.images.logo} alt="" />
-        </div>
-        <div className="login-signup-adjust">
-            <nav className="login-signup">
-                <div className="style-login-signup style-login" >
-                    <Link to="/login">Login</Link>
-                </div>
-                <div className="style-login-signup style-signup">
-                    <Link to="/signup">Sign up!</Link>
-                </div>
-            </nav>
-        </div>
-    </div>
-);
+// const Greeting = () => (
+//     <div className="tip_top_logo">
+//         <div className="all-dem-px">
+//             <img src={window.images.logo} alt="" />
+//         </div>
+//         <div className="login-signup-adjust">
+//             <nav className="login-signup">
+//                 <div className="style-login-signup style-login" >
+//                     <Link to="/login">Login</Link>
+//                 </div>
+//                 <div className="style-login-signup style-signup">
+//                     <Link to="/signup">Sign up!</Link>
+//                 </div>
+//             </nav>
+//         </div>
+//     </div>
+// );
 
-const PrettyFrontPage = () => {
-    return (
-        <div>
-            <h1 className="main-welcome">Experience and be the lens of inspiration and all that doohickey</h1>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <h3 className="main-welcome-description">Your home away from home</h3>
-            <br />
-            <Link to="/signup">
-                <div className="pretty-front-page-button">
-                    <p>Join All Dem PX</p>
-                </div>
-            </Link>
-        </div>
-    )
 
-}
-
-export default PrettyFrontPage;
-export default Greeting;
+// export default Greeting;
