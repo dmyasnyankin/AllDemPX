858b789578126e9dd066661d8e3fb29f7738255b
Dennis Myasnyankin <dennis415@live.com>
2019-02-01 16:11:02 -0800

    Finish footer styling and add links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,10 +5,23 @@ const Footer = () => {
     return (
         <footer>
             <div className="footer">
-
-                {/* <h1 className="main-welcome">Experience and be the lens of inspiration, sharing it with the world</h1>
-                <h3 className="e">Your home away from home</h3> */}
-
+                <h1 className="footer-title" >Get in touch with the Developer</h1>
+                <div className="footer-logos">
+                    <div className="footer-github">
+                    </div>
+                    <a href={"https://github.com/dmyasnyankin/"}>
+                        <button className="footer-github"><i className="fab fa-github"></i></button>
+                    </a>
+                    <a href={"https://www.linkedin.com/in/dennis-myasnyankin/"}>
+                        <button className="footer-linkedin"><i className="fab fa-linkedin"></i></button>
+                    </a>
+                    <a href={"https://angel.co/dennis-myasnyankin?public_profile=1"}>
+                        <button className="footer-angel"><i className="fab fa-angellist"></i></button>
+                    </a>
+                    <a href="mailto:dmyasnyankin@berkeley.edu">
+                        <button className="footer-email"><i className="far fa-envelope"></i></button>
+                    </a>
+                </div>
             </div>
         </footer>
     )
