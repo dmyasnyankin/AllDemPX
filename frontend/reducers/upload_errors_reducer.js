de2bd73fca28e43d1d054d1af9524afd2ededb48
Dennis Myasnyankin <dennis415@live.com>
2019-02-24 17:48:41 -0800

    Remove console logs

--- frontend/reducers/upload_errors_reducer.js
+++ frontend/reducers/upload_errors_reducer.js
@@ -2,7 +2,6 @@ import {RECEIVE_UPLOAD_ERRORS, RECEIVE_PHOTO} from '../actions/photos_actions';
 
 const uploadErrorsReducer = (state = [], action) => {
     Object.freeze(state);
-    console.log(action)
     switch (action.type) {
         case RECEIVE_UPLOAD_ERRORS:
             return action.errors;
