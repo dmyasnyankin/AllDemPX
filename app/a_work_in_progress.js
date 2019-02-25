e9528c76b7444b712a3d28b01fcbeeb395250da9
Dennis Myasnyankin <dennis415@live.com>
2019-02-24 19:07:23 -0800

    Prepare for heroku fix

--- app/a_work_in_progress.js
+++
@@ -1,8 +0,0 @@
-
-// PREVIOUS index.json.jbuilder file
-// @photos.each do | photo |
-//     json.set! photo.id do
-//     json.extract! photo, : id, : title, : author_id
-// json.image_url url_for(photo.image_url)
-// end
-// end
