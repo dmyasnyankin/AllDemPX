596010401ee7ef4c6eb19d0b348a2997832f9466
Dennis Myasnyankin <dennis415@live.com>
2019-04-17 23:34:48 -0700

    Add author id to comments migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_18_060421) do
+ActiveRecord::Schema.define(version: 2019_04_18_063007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_04_18_060421) do
     t.integer "photo_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id", null: false
     t.index ["photo_id"], name: "index_comments_on_photo_id"
   end
 
