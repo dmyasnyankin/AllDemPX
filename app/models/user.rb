bfb528be809c3e252849f24d892c8c8e6c324abe
Dennis Myasnyankin <dennis415@live.com>
2019-04-23 15:12:22 -0700

    Fix user model comments bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
     after_initialize :ensure_session_token
 
-    has_many :reviews,
+    has_many :comments,
         foreign_key: :author_id
 
     def self.find_by_credentials(email, password)
